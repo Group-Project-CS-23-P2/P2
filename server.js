6689e39e04b82e1d82bca4bda0eaab7cf0858322
Peter Slessing <psless23@student.aau.dk>
2024-04-08 10:09:50 +0200

    Change front page HTML link

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import http from 'http';
 import fs from "fs";
 import url from "url";
 
+const clientHTML = fs.readFileSync("/HTML-Pages/frontpage.html";);
+
 let server = new http.Server();
 server.listen(3430, "localhost", () => {
 	console.log("To connect to the chat, go to http://localhost:3430/");
@@ -12,4 +14,13 @@ server.listen(3430, "localhost", () => {
 server.on("request", (request, response) => {
     //Parse where the request wants to go.
     let pathname = url.parse(request.url).pathname;
+
+    //Standard front page respone
+    if (pathname === "/") { // A request for the chat UI
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        }).end(clientHTML);
+    }
+
+
 })
