2b3bf439c00124952a483facfd5ec2e198706d98
DigitaWings <45568915+DigitaWings@users.noreply.github.com>
2024-04-11 14:05:40 +0200

    Initial code for algorithm

--- script1.py
+++ script1.py
@@ -1,11 +1,27 @@
 import sys
 import numpy as np
 import scipy as sp
+import json
 
 # Get the arguments passed from Node.js
 args_from_nodejs = sys.argv[1:]
 
 username = args_from_nodejs[0];
-
+userphysical = args_from_nodejs[1];
+usercreative = args_from_nodejs[2];
+userbrainy = args_from_nodejs[3];
+usersocial = args_from_nodejs[4];
+usercompetitive = args_from_nodejs[5];
+activities = args_from_nodejs[6:];
 # Your Python script logic here
-print("Arguments received from Node.js:", args_from_nodejs)
+
+def costFunction(userfeatures):
+    ratingsum = 0;
+
+    for i in range(len(activities)):
+        ratingsum += np.power((userfeatures.dot(np.array(json.loads(activities[i])["listofFeatures"]))) - activitiesRating,2);
+    
+    quizdiff = (0);
+    return (1/((len(activities)+1)*2))*(ratingsum + quizdiff);
+
+print("Arguments received from Node.js:", args_from_nodejs, costFunction(np.array([5,5,5,5,5])))
