dbced69794289fcdbf15d24bff3f734846603eb3
Anton <amve23@student.aau.dk>
2024-04-11 11:00:39 +0200

    comments

--- db.js
+++ db.js
@@ -18,6 +18,7 @@ DBConnection.connect(err => {
 
 
 /*
+//Table creation function
 
   const createTableQuery = `
   CREATE TABLE IF NOT EXISTS new_User_table (
@@ -42,9 +43,10 @@ DBConnection.query(createTableQuery, (err, results) => {
   console.log('new_User_table created successfully or already exists');
   
 });
+*/
 
-
-
+/*
+//User insert 
   function insertIntoTable(User_id, Username, Password, Age, Physical, Creative, Brainy, Social, Competative, Pricepoint) {
     const query = 'INSERT INTO `new_User_table` (`User_id`, `Username`, `Password`,`Age`,`Physical`,`Creative`,`Brainy`, `Social`, `Competative`, `Pricepoint`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
     DBConnection.query(query, [User_id, Username, Password, Age, Physical, Creative, Brainy, Social, Competative, Pricepoint], (err, results) => {
@@ -57,8 +59,31 @@ DBConnection.query(createTableQuery, (err, results) => {
   }
 
   insertIntoTable(1, 'Admin', `Password`, 22, 5, 3, 5, 4, 5, 300);
+  
   */
  
+
+  /*
+//Activity insert 
+  function insertIntoTable(Activity_id, Activity_name, Physical_rank, Creative_rank, Brainy_rank, Social_rank, Competative_rank, Pricepoint) {
+    const query = 'INSERT INTO `new_User_table` (Activity_id`, `Activity_name`, `Physical_rank`,`Creative_rank`,`Brainy_rank`,`Social_rank`,`Competative_rank`, `Pricepoint`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    DBConnection.query(query, [Activity_id, Activity_name, Physical_rank, Creative_rank, Brainy_rank, Social_rank, Competative_rank, Pricepoint], (err, results) => {
+      if (err) {
+        console.error('Error inserting data into table', err);
+        return;
+      }
+      console.log('Data inserted successfully:', results);
+    });
+  }
+
+  insertIntoTable(1, 'Admin', `Password`, 22, 5, 3, 5, 4, 5, 300);
+  
+  */
+ 
+
+  /*
+  //Denne funktion bruges til at fjerne tables!!!
+
   DBConnection.query('DROP TABLE Activity_table', (err) => {
     if (err) {
       console.error('Error dropping Activity_table', err);
@@ -67,6 +92,8 @@ DBConnection.query(createTableQuery, (err, results) => {
     console.log('Activity_table dropped successfully');
   });
   
+*/
+
 
   DBConnection.query('SHOW TABLES', (err, results) => {
     if (err) {
